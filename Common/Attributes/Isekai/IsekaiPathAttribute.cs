ad55940822fc18ade3dac6002be9ff9ca015bd32
Jerrod Sunderland <jerrod.sunderland@gmail.com>
2025-12-22 23:28:08 -0500

    update path attribute

--- Common/Attributes/Isekai/IsekaiPathAttribute.cs
+++ Common/Attributes/Isekai/IsekaiPathAttribute.cs
@@ -1,7 +1,17 @@
 namespace Common.Attributes.Isekai;
 
+public enum IsekaiHttpMethod
+{
+    Get,
+    Post,
+    Put,
+    Delete,
+    Patch
+}
+
 [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-public sealed class IsekaiPathAttribute(string path) : Attribute
+public sealed class IsekaiPathAttribute(string path, IsekaiHttpMethod method = IsekaiHttpMethod.Get) : Attribute
 {
     public string Path { get; } = path;
+    public IsekaiHttpMethod Method { get; } = method;
 }
