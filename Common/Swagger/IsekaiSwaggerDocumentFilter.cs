ca74abb7a5a6973e04c97cd498e93419607cd5c8
Jerrod Sunderland <jerrod.sunderland@gmail.com>
2025-12-22 23:53:54 -0500

    format

--- Common/Swagger/IsekaiSwaggerDocumentFilter.cs
+++ Common/Swagger/IsekaiSwaggerDocumentFilter.cs
@@ -1,61 +1,60 @@
+namespace Common.Swagger;
+
 using System.Reflection;
 using Common.Attributes.Isekai;
 using Common.Services.Isekai;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace Common.Swagger
+public class IsekaiSwaggerDocumentFilter(Assembly clientAssembly) : IDocumentFilter
 {
-    public class IsekaiSwaggerDocumentFilter : IDocumentFilter
+    private readonly Assembly _clientAssembly = clientAssembly;
+
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
     {
-        private readonly Assembly _clientAssembly;
+        var isekaiTypes = _clientAssembly
+            .GetTypes()
+            .Where(t => t.IsInterface && typeof(IIsekaiService).IsAssignableFrom(t) && t != typeof(IIsekaiService));
 
-        public IsekaiSwaggerDocumentFilter(Assembly clientAssembly)
+        foreach (var type in isekaiTypes)
         {
-            _clientAssembly = clientAssembly;
-        }
-
-        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
-        {
-            var isekaiTypes = _clientAssembly
-                .GetTypes()
-                .Where(t => t.IsInterface && typeof(IIsekaiService).IsAssignableFrom(t) && t != typeof(IIsekaiService));
-
-            foreach (var type in isekaiTypes)
+            foreach (var method in type.GetMethods())
             {
-                foreach (var method in type.GetMethods())
+                var pathAttr = method.GetCustomAttribute<IsekaiPathAttribute>();
+                if (pathAttr == null)
                 {
-                    var pathAttr = method.GetCustomAttribute<IsekaiPathAttribute>();
-                    if (pathAttr == null)
-                        continue;
-
-                    var path = pathAttr.Path.StartsWith('/') 
-                        ? pathAttr.Path 
-                        : "/" + pathAttr.Path;
-
-                    // Map your enum to Swagger OperationType
-                    var operationType = pathAttr.Method switch
-                    {
-                        IsekaiHttpMethod.Get => OperationType.Get,
-                        IsekaiHttpMethod.Post => OperationType.Post,
-                        IsekaiHttpMethod.Put => OperationType.Put,
-                        IsekaiHttpMethod.Delete => OperationType.Delete,
-                        IsekaiHttpMethod.Patch => OperationType.Patch,
-                        _ => OperationType.Get
-                    };
-
-                    if (!swaggerDoc.Paths.ContainsKey(path))
-                        swaggerDoc.Paths[path] = new OpenApiPathItem();
-
-                    swaggerDoc.Paths[path].Operations[operationType] = new OpenApiOperation
-                    {
-                        Summary = $"Isekai: {type.Name}.{method.Name}",
-                        Responses =
-                        {
-                            ["200"] = new OpenApiResponse { Description = "Success" }
-                        }
-                    };
+                    continue;
                 }
+
+                var path = pathAttr.Path.StartsWith('/')
+                    ? pathAttr.Path
+                    : "/" + pathAttr.Path;
+
+                // Map your enum to Swagger OperationType
+                var operationType = pathAttr.Method switch
+                {
+                    IsekaiHttpMethod.Get => OperationType.Get,
+                    IsekaiHttpMethod.Post => OperationType.Post,
+                    IsekaiHttpMethod.Put => OperationType.Put,
+                    IsekaiHttpMethod.Delete => OperationType.Delete,
+                    IsekaiHttpMethod.Patch => OperationType.Patch,
+                    _ => OperationType.Get
+                };
+
+                if (!swaggerDoc.Paths.TryGetValue(path, out var value))
+                {
+                    value = new OpenApiPathItem();
+                    swaggerDoc.Paths[path] = value;
+                }
+
+                value.Operations[operationType] = new OpenApiOperation
+                {
+                    Summary = $"Isekai: {type.Name}.{method.Name}",
+                    Responses =
+                    {
+                        ["200"] = new OpenApiResponse { Description = "Success" }
+                    }
+                };
             }
         }
     }
