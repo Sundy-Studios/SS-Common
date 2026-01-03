43d1d9e968f6533e073cdaee475e12d687249cc7
Jerrod Sunderland <jerrod.sunderland@gmail.com>
2026-01-03 15:19:31 -0500

    setup sumo logging

---
+++ Common/Logging/SumoLoggingExtensions.cs
@@ -0,0 +1,35 @@
+namespace SS.Common.Logging;
+
+using Microsoft.AspNetCore.Builder;
+using Serilog;
+using Serilog.Sinks.SumoLogic;
+
+public static class SumoLoggingExtensions
+{
+    public static WebApplicationBuilder AddSumoLogging(
+        this WebApplicationBuilder builder,
+        string? sumoUrl)
+    {
+        var loggerConfig = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("org", "SS")
+            .Enrich.WithProperty("service", builder.Environment.ApplicationName)
+            .Enrich.WithProperty("env", builder.Environment.EnvironmentName);
+
+        if (!string.IsNullOrWhiteSpace(sumoUrl))
+        {
+            loggerConfig = loggerConfig
+                .WriteTo.SumoLogic(sumoUrl);
+        }
+
+        // Always log to console
+        loggerConfig = loggerConfig.WriteTo.Console();
+
+        Log.Logger = loggerConfig.CreateLogger();
+
+        builder.Host.UseSerilog();
+
+        return builder;
+    }
+}
